cff0e1ad45e97e48d1537f132e39221e7f5ef392
David Martinson <davidmartinson@Davids-Mac-mini.local>
2023-10-15 14:30:49 -0700

    reducer and context

--- src/reducers/parts_reducer.jsx
+++ src/reducers/parts_reducer.jsx
@@ -4,12 +4,26 @@ import {
     GET_PARTS_ERROR
 } from '../actions'
 
-const parts_reducer = (state, acton) => {
+const parts_reducer = (state, action) => {
 
     if(action.type === GET_PARTS_BEGIN) {
         return {
             ...state,
             parts_loading: true
+        }
+    }
+    if(action.type === GET_PARTS_SUCCESS){
+        return {
+            ...state,
+            parts_loading: false,
+            parts: action.payload
+        }
+    }
+    if(action.type === GET_PARTS_ERROR){
+        return {
+            ...state,
+            parts_error: true,
+            parts_loading: false
         }
     }
 
