6ad255a19a0561e9d87bdd7044cbc17b2f180850
David Martinson <davidmartinson@Davids-Mac-mini.local>
2023-10-15 18:31:21 -0700

    filter fix

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import MobileNav from "./MobileNav";
 import Navbar from "./Navbar";
 import Part from "./Part";
 import PartsList from "./PartsList";
+import Filters from './Filters'
 
 export {
     MobileNav,
     Navbar,
     Part,
-    PartsList
+    PartsList,
+    Filters
 }
