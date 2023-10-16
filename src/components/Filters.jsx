baef85f8649bf8aca5db120caf19eb0d756d5921
David Martinson <davidmartinson@Davids-Mac-mini.local>
2023-10-16 01:02:56 -0700

    filters and sorting

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,10 +1,29 @@
 import styled from 'styled-components'
-
+import Sort from './Sort'
+import { useFilterContext } from '../context/filter_context'
 
 const Filters = () => {
+
+  const {
+    filters: {
+      min_price,
+      max_price,
+      mfg,
+      part_type
+    },
+    updateFilters,
+    clearFilters,
+    all_parts
+  } = useFilterContext()
+
+  const mfgs = getUniqueValues(all_parts, 'mfg')
+  const part_types = getUniqueValues(all_parts, 'part_type')
+
   return (
     <Wrapper>
-        <div>Filters</div>
+        <div className="test">
+          <Sort />
+        </div>
     </Wrapper>
   )
 }
